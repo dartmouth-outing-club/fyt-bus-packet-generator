f9b0005ae28e10519e450f9988d582ee3cefe155
Alexander Petros <apetros15@gmail.com>
2022-06-18 09:14:20 -0400

    Serve basic JSON over HTTP

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import { getDirections } from './clients/google-client.js'
+import http from 'http'
+import fs from 'fs/promises'
 
-const HANOVER_COORDINATES = '44.875039,-71.05471'
-const LODJ_COORDINATES = '43.977253,-71.8154831'
-const ANDROSCOGGIN_COORDINATES = '44.714042,-71.173832'
+const port = process.env.PORT || 3000
+const host = 'localhost'
 
-const directions = await getDirections([
-  HANOVER_COORDINATES,
-  LODJ_COORDINATES,
-  ANDROSCOGGIN_COORDINATES
-])
+const API_RESPONSE_FILE = './samples/sample-directions-api-response.json'
+const RESPONSE_JSON = await fs.readFile(API_RESPONSE_FILE, { encoding: 'utf8' }).then(file => file.toString())
 
-console.log(JSON.stringify(directions))
+const server = http.createServer((req, res) => {
+  const requestUrl = new URL(req.url, `http://${req.headers.host}`)
+  console.log(`Request receieved for ${requestUrl}`)
+
+  res.setHeader('Content-Type', 'application/json')
+  res.statusCode = 200
+  res.write(RESPONSE_JSON)
+  res.end()
+})
+
+server.listen(port, host, () => {
+  console.log(`It's giving... http://${host}:${port}`)
+})
