cfdcbd637716f7b3bd75458989b2a6136f9a1e29
Alexander Petros <apetros15@gmail.com>
2022-06-29 17:39:01 -0400

    Enhance init script

--- scripts/db-schema.sql
+++ scripts/db-schema.sql
@@ -1,19 +1,31 @@
 CREATE TABLE stops (
-    coordinates TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    address TEXT,
-    special_instructions TEXT
+	coordinates TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	address TEXT,
+	special_instructions TEXT
 );
 
 CREATE TABLE directions (
-    id INTEGER PRIMARY KEY,
-    start_coordinates TEXT REFERENCES stops ON UPDATE SET NULL,
-    end_coordinates TEXT REFERENCES stops ON UPDATE SET NULL,
-    google_directions TEXT,
-    updated_at INTEGER
+	id INTEGER PRIMARY KEY,
+	start_coordinates TEXT REFERENCES stops ON UPDATE SET NULL,
+	end_coordinates TEXT REFERENCES stops ON UPDATE SET NULL,
+	google_directions TEXT,
+	updated_at INTEGER
 );
 
-CREATE TRIGGER last_updated_trigger AFTER UPDATE ON directions
+CREATE TABLE packets (
+	name TEXT PRIMARY KEY,
+	html_content TEXT,
+	updated_at INTEGER
+)
+
+
+CREATE TRIGGER directions_updated AFTER UPDATE ON directions
 BEGIN
-    UPDATE directions SET updated_at = unixepoch() WHERE id = NEW.id;
+	UPDATE directions SET updated_at = unixepoch() WHERE id = NEW.id;
+END;
+
+CREATE TRIGGER packets_updated AFTER UPDATE ON directions
+BEGIN
+	UPDATE packets SET updated_at = unixepoch() WHERE name = NEW.name;
 END;
--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-#Run this from the root directory
+#Run this from the root directory, via `npm run init`
 set -e
+DB_FILENAME="packet-generator.db"
+CSV_FILENAME="stops.csv"
 
-sqlite3 packet-generator.db << EOF
+if [[ -f $DB_FILENAME ]]
+then
+	echo "Error - $DB_FILENAME already exists"
+	exit 1
+fi
+
+echo "Creating $DB_FILENAME from $CSV_FILENAME"
+sqlite3 $DB_FILENAME << EOF
 .read ./scripts/db-schema.sql
-.import --csv --skip 1 ./scripts/stops.csv stops
+.import --csv --skip 1 ./scripts/$CSV_FILENAME stops
 EOF
 
+# set -e ensures we only see this if sqlite3 exited successfully
+echo 'Import completed successfully'
+exit 0
